479e16a48292a32000595bbd6c1d479b39f6446c
phpmontrealconvelio <phpmontreal+convelio@gmail.com>
2021-11-22 12:15:10 +0100

    Placeholders and interpelation system.

--- src/TemplateManager.php
+++ src/TemplateManager.php
@@ -2,6 +2,60 @@
 
 class TemplateManager
 {
+    // Placeholders.
+    private $placeholders = [];
+
+    // To set a defualt value if the replacement text does NOT exixts (default empty strings).
+    private $emptyPlaceholdersDefaultTexts = [
+        'ALL' => '',
+    ];
+
+    public function __construct()
+    {
+        // Return placeholders
+        $this->register('quote:destination_name', function (Quote $quote) {
+            return DestinationRepository::getInstance()->getById($quote->destinationId);
+        });
+    }
+
+    // Register functions.
+    private function register(string $placeholder, callable $fn, bool $defaultEmptyString = false)
+	{
+        $this->placeholders[$placeholder] = [
+            'placeholder' => $placeholder,
+            'defaultBlank' => $defaultEmptyString,
+            'func' => function ($value) use ($fn, $defaultEmptyString) {
+                try {
+                    return (string) call_user_func($fn, $value);
+                } catch (TypeError $e) {
+                    return ($defaultEmptyString)
+                        ? $this->emptyPlaceholdersDefaultTexts['ALL']
+                        : $value
+                    ;
+                }
+            },
+        ];
+	}
+
+    // Match
+    private function matchInterpelations(string $text, array $parameters)
+	{
+        preg_match_all('/\[(?<placeholder>(?<var>\w+)(:.+?)?)\]/', $text, $matches, PREG_SET_ORDER);
+        $replacements = [];
+        foreach ($matches as $match) {
+            $default = $match[0];
+            $value = $parameters[$match['var']];
+            $replacements[$default] = $this->interpelate($match['placeholder'], $value);
+        }
+        return $replacements;
+	}
+
+    // Reflect
+    private function interpelate($placeholder, $value)
+	{
+        return (string) call_user_func($this->placeholders[$placeholder]['func'], $value);
+	}
+
     public function getTemplateComputed(Template $tpl, array $data)
     {
         if (!$tpl) {
@@ -23,6 +77,15 @@ class TemplateManager
         Use preg_match_all for matching.
         Define interpelation functions.
         Add verifications.
+        */
+
+        /*
+        $data = $this->checkParameters($text, $data);
+        $interpellations = $this->matchInterpelations($text, $data);
+
+        return strtr($text, $interpellations);
+
+        exit;
         */
 
         $APPLICATION_CONTEXT = ApplicationContext::getInstance();
